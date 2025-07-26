781b0a7e7ab569ee3304e878c1799e4f4788e59a
myusuke <muzin00@gmail.com>
2025-07-26 14:05:12 +0900

    Refactor server main function to handle incoming connections and read messages

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,19 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
 fn main() {
-    println!("Hello, server!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        thread::spawn(move || {
+            handle_connection(stream.unwrap());
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut msg = String::new();
+    stream.read_to_string(&mut msg).unwrap();
+    println!("Received message: {}", msg);
 }
